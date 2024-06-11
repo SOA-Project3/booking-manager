f87fd28dd2fbba194e553d2b94bb82210a681bcb
valesskablanco <valesskablanco@estudiantec.cr>
2024-06-10 20:05:40 -0600

    test

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ const { PubSub } = require('@google-cloud/pubsub');
 const sql = require("mssql");
 const bodyParser = require('body-parser');
 const port = 3000; 
+const crypto = require('crypto');
 
 const app = express(); //Main express app
 const router = express.Router(); 
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(bodyParser.json());
 
+
+const algorithm = 'aes-256-cbc'; // Using AES encryption
+const key = crypto.randomBytes(32); // Generate a 32 byte key
+const iv = crypto.randomBytes(16); // Generate a 16 byte IV
+
+function encrypt(text) {
+  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+const password = 'your-password';
+const encrypted = encrypt(password);
+
+console.log("Encrypted password:", encrypted);
 
 
 // SQL Server configuration
