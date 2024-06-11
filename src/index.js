5e52bd07f2039314a277d10cb4ffbf4c87baec0d
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-06-10 22:04:01 -0600

    delete testin

--- src/index.js
+++ src/index.js
@@ -21,23 +21,6 @@ var config = {
   }
 };
 
-const algorithm = 'aes-256-cbc'; // Using AES encryption
-const key = crypto.randomBytes(32); // Generate a 32 byte key
-
-function decrypt(iv, encryptedText) {
-  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-  let decrypted = decipher.update(encryptedText);
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-  return decrypted.toString();
-}
-
-const iv = 'b14e97e1857e62b9ebf3d251484e8053';
-const encryptedText = '6e9b8ee0b29d9e07d0680e844aee4465';
-
-const decrypted = decrypt(iv, encryptedText);
-
-console.log("Decrypted password:", decrypted);
-
 // Connect to SQL Server
 sql.connect(config, err => {
   if (err) {
